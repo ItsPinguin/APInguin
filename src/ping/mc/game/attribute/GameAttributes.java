e6e06d7dc957271e5205303cda0fa7cb74eec58e
ItsPinguin <pingdevyt@gmail.com>
2023-05-18 14:20:33 +0200

    -added GameAttributeBuilder -ADD_MULTIPLY -> ADD_PERCENT
    Signed-off-by: ItsPinguin <pingdevyt@gmail.com>

--- src/ping/mc/game/attribute/GameAttributes.java
+++ src/ping/mc/game/attribute/GameAttributes.java
@@ -3,6 +3,12 @@ package ping.mc.game.attribute;
 import java.util.HashMap;
 
 public class GameAttributes {
+    private static GameAttributeBuilder attributeBuilder=new GameAttributeBuilder() {
+        @Override
+        public String build(GameAttributeModifier gameAttributeModifier) {
+            return GameAttributeBuilder.super.build(gameAttributeModifier);
+        }
+    };
     static HashMap<String, GameAttribute> attributes=new HashMap<>();
     public static void addAttribute(GameAttribute attribute){
         attributes.put(attribute.getId(), attribute);
@@ -10,5 +16,13 @@ public class GameAttributes {
 
     public static GameAttribute getAttribute(String key){
         return attributes.get(key);
+    }
+
+    public static GameAttributeBuilder getAttributeBuilder() {
+        return attributeBuilder;
+    }
+
+    public static void setAttributeBuilder(GameAttributeBuilder attributeBuilder) {
+        GameAttributes.attributeBuilder = attributeBuilder;
     }
 }
