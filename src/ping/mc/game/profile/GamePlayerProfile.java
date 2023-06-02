dacf59bb640d88ee26e813a0f40f1718915f1f65
ItsPinguin <pingdevyt@gmail.com>
2023-06-02 19:16:26 +0200

    -remade profiles, so they're saved as an object and not json
    Signed-off-by: ItsPinguin <pingdevyt@gmail.com>

--- src/ping/mc/game/profile/GamePlayerProfile.java
+++ src/ping/mc/game/profile/GamePlayerProfile.java
@@ -1,22 +1,40 @@
 package ping.mc.game.profile;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import ping.utils.Config;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class GamePlayerProfile implements Serializable {
     UUID uuid;
-    String currentProfile;
-    List<String> profiles=new ArrayList<>();
+    UUID currentProfile;
+    List<UUID> profiles=new ArrayList<>();
+
+    public GamePlayerProfile(UUID uuid) {
+        this.uuid = uuid;
+        if (new File(Config.PLAYER_PROFILES_DIRECTORY+uuid).exists()){
+            try {
+                GamePlayerProfile profile= (GamePlayerProfile) new ObjectInputStream(new FileInputStream(Config.PLAYER_PROFILES_DIRECTORY + uuid)).readObject();
+                currentProfile=profile.currentProfile;
+                profiles=profile.profiles;
+            } catch (IOException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     public void save(){
         try {
+            GameProfile profile= GameProfiles.profiles.get(uuid);
+            Player player= Bukkit.getPlayer(uuid);
+            assert player != null;
+            profile.armorContent=player.getInventory().getArmorContents();
+            profile.itemContent=player.getInventory().getContents();
+            profile.extraItemContent=player.getInventory().getExtraContents();
             new ObjectOutputStream(new FileOutputStream(Config.PLAYER_PROFILES_DIRECTORY+uuid)).writeObject(this);
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/ping/mc/game/profile/GameProfiles.java
+++ src/ping/mc/game/profile/GameProfiles.java
@@ -1,4 +1,9 @@
 package ping.mc.game.profile;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class GameProfiles {
+    public static HashMap<UUID, GameProfile> profiles=new HashMap<>();
+    public static HashMap<UUID, GamePlayerProfile> playerProfiles=new HashMap<>();
 }
