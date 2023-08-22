62036519ae76f85efd5dd8ed8c18b39db8896166
ItsPinguin <pingdevyt@gmail.com>
2023-08-22 15:53:35 +0200

    🔥| modified addon structure
    Signed-off-by: ItsPinguin <pingdevyt@gmail.com>

--- src/ping/apinguin/Registries.java
+++
@@ -1,4 +0,0 @@
-package ping.apinguin;
-
-public class Registries {
-}
--- src/ping/mc/game/item/GameItemBuilder.java
+++
@@ -1,7 +0,0 @@
-package ping.mc.game.item;
-
-import org.bukkit.inventory.ItemStack;
-
-public interface GameItemBuilder {
-    default ItemStack build(ItemStack itemStack){return itemStack;}
-}
